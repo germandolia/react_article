35618dfc740dc073d7b8cd5bd7b13ccf3fb66498
German <germandolia@gmail.com>
2020-06-22 16:47:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.cdcdd76b4eb4a8e46d5490d58f413417.js"
 );
 
 self.addEventListener('message', (event) => {
